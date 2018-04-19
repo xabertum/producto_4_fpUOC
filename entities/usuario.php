d4ed6b93539a19f89d9098bd1e9d6b268ed3099e
xabertum <mohatar@gmail.com>
2018-04-19 15:53:11 +0200

    Added Getters & Setters

--- entities/usuario.php
+++ entities/usuario.php
@@ -1,3 +1,97 @@
 <?php
 
+class Usuario {
+    
+    // Variables internas
+    
+    private $id;
+    private $username;
+    private $password;
+    private $rol;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Getters & Setters
+    
+    
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRol()
+    {
+        return $this->rol;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param mixed $rol
+     */
+    public function setRol($rol)
+    {
+        $this->rol = $rol;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+}
 
