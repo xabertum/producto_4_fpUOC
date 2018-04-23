712d71638d46f75b4b731d93591199dcdffe9778
xabertum <mohatar@gmail.com>
2018-04-23 20:15:57 +0200

    Added AJAX validation

--- vista/js/app.js
+++ vista/js/app.js
@@ -1,6 +1,24 @@
-$(document).ready(function) {
+
+$(document).ready(function() {
 	
-	alert ("as");
+	$("#loginForm").bind("submit", function () {
+		
+		
+		$.ajax({
+			type: $(this).attr("method"),
+			url: $(this).attr("action"),
+			data: $(this).serialize(),
+			success:function() {
+				alert("conectado");
+			},
+			error: function () {
+				alert("error");
+			}
+		});
+		
+		return false
+		
+	});
 	
 	
-}
+});
