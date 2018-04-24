b7a6975ee0431023078cfa7bfde13b9ce662d219
xabertum <mohatar@gmail.com>
2018-04-24 13:37:29 +0200

    Added getUÑser() function

--- controlador/UsuarioControlador.php
+++ controlador/UsuarioControlador.php
@@ -1,23 +1,28 @@
-<?php 
-
-
+<?php
 include '../datos/usuarioDAO.php';
 
-class UsuarioControlador {
-    
-    public static function login ($usuario, $password) {
-        
+class UsuarioControlador
+{
+
+    public static function login ($usuario, $password)
+    {
         $obj_usuario = new Usuario();
         $obj_usuario->setUsername($usuario);
         $obj_usuario->setPassword($password);
         
         return UsuarioDAO::login($obj_usuario);
+    }
+
+    public function getUser ($usuario, $password)
+    {
         
+        $obj_usuario = new Usuario();
+        $obj_usuario->setUsername($usuario);
+        $obj_usuario->setPassword($password);
+        
+        return UsuarioDAO::getUser($obj_usuario);
         
         
         
     }
-    
-    
-    
 }
