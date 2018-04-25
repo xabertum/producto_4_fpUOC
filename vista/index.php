6510bc6d034e31024e1170a3f1628727d41d79a4
xabertum <mohatar@gmail.com>
2018-04-25 16:17:39 +0200

    Minor warnings fixed

--- vista/index.php
+++ vista/index.php
@@ -252,7 +252,7 @@
 						<ul class="footer-ul">
 							<li>About Us</li>
 							<li>Contact</li>
-							<li>Terms & Conditions</li>
+							<li>Terms &amp; Conditions</li>
 						</ul>
 					</div>
 					<div class="col-3">
@@ -280,13 +280,8 @@
     ================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
 
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-		crossorigin="anonymous"></script>
-	<script
-		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-		crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/main.js"></script>
 
