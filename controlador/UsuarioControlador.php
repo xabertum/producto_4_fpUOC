b00025ee1f2b637219fcf4e8a82e4903c389dfd5
xabertum <mohatar@gmail.com>
2018-05-16 13:19:06 +0200

    Added Files

--- controlador/UsuarioControlador.php
+++ controlador/UsuarioControlador.php
@@ -4,25 +4,34 @@ include '../datos/usuarioDAO.php';
 class UsuarioControlador
 {
 
-    public static function login ($usuario, $password)
+    public static function login($usuario, $password)
     {
         $obj_usuario = new Usuario();
         $obj_usuario->setUsername($usuario);
         $obj_usuario->setPassword($password);
-        
+
         return UsuarioDAO::login($obj_usuario);
     }
 
-    public static function getUser ($usuario, $password)
+    public static function getUser($usuario, $password)
     {
-        
+
         $obj_usuario = new Usuario();
         $obj_usuario->setUsername($usuario);
         $obj_usuario->setPassword($password);
-        
+
         return UsuarioDAO::getUser($obj_usuario);
-        
-        
-        
+
+    }
+
+    public static function registrar($nombre, $username, $password, $rol)
+    {
+        $obj_usuario = new Usuario();
+        $obj_usuario->setNombre($nombre);
+        $obj_usuario->setUsername($username);
+        $obj_usuario->setPassword($password);
+
+        return UsuarioDAO::registrar($obj_usuario);
+
     }
 }
