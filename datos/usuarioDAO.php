edd04ee68cb60820434fc9c2ceeaf70235b4ca02
xabertum <mohatar@gmail.com>
2018-05-16 12:51:22 +0200

    Added Registration

--- datos/usuarioDAO.php
+++ datos/usuarioDAO.php
@@ -7,14 +7,14 @@ class UsuarioDAO extends Conexion
 
     protected static $cnx;
 
-    private static function getConexion ()
+    private static function getConexion()
     {
         self::$cnx = Conexion::conectar();
     }
 
-    private static function desconectar ()
+    private static function desconectar()
     {
-        self::$cnx = NULL;
+        self::$cnx = null;
     }
 
     /**
@@ -24,68 +24,99 @@ class UsuarioDAO extends Conexion
      * @param object $usuario
      * @return boolean
      */
-    public static function login ($usuario)
+    public static function login($usuario)
     {
         $query = "SELECT * FROM usuarios WHERE username = :username AND password = :password";
-        
+
         self::getConexion();
-        
+
         $resultado = self::$cnx->prepare($query);
-        
+
         $_usuario = $usuario->getUsername();
         $_password = $usuario->getPassword();
-        
+
         $resultado->bindParam(":username", $_usuario);
         $resultado->bindParam(":password", $_password);
-        
+
         $resultado->execute();
-        
+
         if ($resultado->rowCount() > 0) {
-            
+
             $filas = $resultado->fetch();
             if ($filas['username'] == $usuario->getUsername() &&
-                    $filas['password'] == $usuario->getPassword()) {
-                
+                $filas['password'] == $usuario->getPassword()) {
+
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     /**
-     * 
+     *
      * @param object $usuario
      * @return object
      */
-    public static function getUser ($usuario)
+    public static function getUser($usuario)
     {
         $query = "SELECT * FROM usuarios WHERE username = :username AND password = :password";
-        
+
         self::getConexion();
-        
+
         $resultado = self::$cnx->prepare($query);
-        
+
         $_usuario = $usuario->getUsername();
         $_password = $usuario->getPassword();
-        
+
         $resultado->bindParam(":username", $_usuario);
         $resultado->bindParam(":password", $_password);
-        
+
         $resultado->execute();
-        
+
         $filas = $resultado->fetch();
-        
+
         $usuario = new Usuario();
         $usuario->setId($filas["id"]);
         $usuario->setNombre($filas["nombre"]);
         $usuario->setRol($filas["rol"]);
         $usuario->setUsername($filas["username"]);
-        
+
         return $usuario;
-        
+
     }
-    
-    
-    
+
+/**
+ *
+ * @param object $usuario
+ * @return object
+ */
+    public static function registrar($usuario)
+    {
+        $query = "SELECT * FROM usuarios WHERE username = :username AND password = :password";
+
+        self::getConexion();
+
+        $resultado = self::$cnx->prepare($query);
+
+        $_usuario = $usuario->getUsername();
+        $_password = $usuario->getPassword();
+
+        $resultado->bindParam(":username", $_usuario);
+        $resultado->bindParam(":password", $_password);
+
+        $resultado->execute();
+
+        $filas = $resultado->fetch();
+
+        $usuario = new Usuario();
+        $usuario->setId($filas["id"]);
+        $usuario->setNombre($filas["nombre"]);
+        $usuario->setRol($filas["rol"]);
+        $usuario->setUsername($filas["username"]);
+
+        return $usuario;
+
+    }
+
 }
