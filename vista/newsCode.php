f7576ba7949756d50b21ae3a410f300f353bdbd2
xabertum <mohatar@gmail.com>
2018-05-20 16:16:24 +0200

    testing overhang.js

--- vista/newsCode.php
+++ vista/newsCode.php
@@ -1,7 +1,20 @@
 <?php
 include '../controlador/UsuarioControlador.php';
-include '../helps/helps.php';
+
+header('Content-type: application/json');
+
+$resultado = array();
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    
+    if (isset($_POST['editor1']) && isset($_POST['editor2'])) {
+
+        $data_editor1 = $_POST['editor1'];
+        $data_editor2 = $_POST['editor2'];
+
+        $resultado = array(
+            "estado" => "true"
+        );
+
+        return print(json_encode($resultado));
+    }
 }
