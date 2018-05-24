c2e8b1124bbe7cac570bcb2d493d594440f902ec
xabertum <mohatar@gmail.com>
2018-05-24 09:29:24 +0200

    Minor changes

--- vista/news-index.php
+++ vista/news-index.php
@@ -189,14 +189,12 @@ $noticias = $resultado->fetchAll(PDO::FETCH_ASSOC);
         <script src="https://cdn.ckeditor.com/4.9.2/standard-all/ckeditor.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
             crossorigin="anonymous"></script>
 
         <!-- CUSTOM SCRIPTS -->
         <script src="js/overhang.js"></script>
         <script src="js/app.js"></script>
-
         <script src="js/bootstrap.min.js"></script>
 
     </body>
